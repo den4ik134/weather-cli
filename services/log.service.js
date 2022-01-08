da686d4c1fd35aa82c2056174d7e669e9ee42aa3
Denys Bilyk <bilko.dp@gmail.com>
2022-01-08 21:10:57 +0200

    Added log.service for fency output in console

---
+++ services/log.service.js
@@ -0,0 +1,26 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+
+const printError = (error) => {
+    console.log(chalk.bgRed(' ERROR ' + '' + error));
+};
+
+const printSuccess = (message) => {
+    console.log(chalk.bgGreen(' ERROR ' + '' + message));
+};
+
+const printHelp = () => {
+    console.log(
+        dedent(`
+        ${chalk.bgCyan(' HELP ')}
+        Without parameters - output weather
+        -s [CITY] for city install
+        -h for output help
+        -t [API_KEY] for token save
+        `)
+    );
+};
+
+
+export {printError, printSuccess, printHelp};
--- weather.js
+++ weather.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
-import {getArgs} from './helpers/args.js'
+import {getArgs} from './helpers/args.js';
+import {printHelp} from './services/log.service.js';
 
 
 const initCLI = () => {
-    const args = getArgs(process.argv) 
-    console.log(args);
+    const args = getArgs(process.argv);
+    if (args.h) {
+        printHelp();
+    }
+    if (args.s) {
+        // Save city
+    }
+    if (args.t) {
+        // Save token
+    }
+    // Output weather
 };
 
 initCLI();
