13302216576690ee8dd52b9f86bf94bc14f9ba6c
Denys Bilyk <bilko.dp@gmail.com>
2022-01-09 14:19:40 +0200

    Added service for set and use API_KEY and getWeather method

---
+++ services/api.service.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+
+const getWeather = async (city) => {
+    const token = await getKeyValue(TOKEN_DICTIONARY.token);
+    if (!token) {
+        throw new Error('Not set API_KEY, set this with command -t [API_KEY]');
+    };
+
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+        params: {
+            q: city,
+            appid: token,
+            lang: 'ru',
+            units: 'metric'
+        }
+    });
+
+    return data;
+};
+
+export { getWeather };
--- weather.js
+++ weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import {getArgs} from './helpers/args.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getWeather } from './services/api.service.js';
 
 const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Token not given!');
+        return;
+    }
     try {
-        await saveKeyValue('token', token);
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
         printSuccess('Token was saved!');
     } catch (error) {
         printError(error.message);
@@ -22,7 +27,7 @@ const initCLI = () => {
     if (args.t) {
         return saveToken(args.t);
     }
-    // Output weather
+    getWeather('dnipro')
 };
 
 initCLI();
